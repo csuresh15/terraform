078fa863cd968fd1d8a956b83b2997b472c7bace
csuresh <sureshchilkuri@gmail.com>
2021-07-26 23:27:08 +0530

    practice1

--- 04-remote-state/ec2-remote_state.tf
+++ 04-remote-state/ec2-remote_state.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "Generic" {
   ami = "ami-074df373d6bafa625"
   instance_type = "t3.micro"
-}
-  tags   = {
+  tags = {
     Name = "Generic"
   }
+}
   provider "aws" {
     region = "us-east-1"
   }
