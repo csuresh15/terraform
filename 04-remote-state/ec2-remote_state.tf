cfd494a3763b63cdc31b6eb6f33f5dcbc8d9e376
csuresh <sureshchilkuri@gmail.com>
2021-07-26 23:24:43 +0530

    practice1

---
+++ 04-remote-state/ec2-remote_state.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "Generic" {
+  ami = "ami-074df373d6bafa625"
+  instance_type = "t3.micro"
+}
+  tags   = {
+    Name = "Generic"
+  }
+  provider "aws" {
+    region = "us-east-1"
+  }
+  terraform {
+    backend "s3" {}
+    bucket = "terraform-st-state"
+    key    = "Generic/terraform.tfstate"
+    region = "us-east-1"
+  }
