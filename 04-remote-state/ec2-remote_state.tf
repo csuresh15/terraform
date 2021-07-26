37eb4c382560b5b5f8127ba77612b4fc1779582a
csuresh <sureshchilkuri@gmail.com>
2021-07-26 23:51:58 +0530

    practice1

--- 04-remote-state/ec2-remote_state.tf
+++ 04-remote-state/ec2-remote_state.tf
@@ -5,13 +5,33 @@ resource "aws_instance" "Generic" {
      Name       = "Generic"
   }
 }
-  provider "aws" {
-    region      = "us-east-1"
+//  provider "aws" {
+//    region      = "us-east-1"
+//  }
+//  terraform {
+//    backend "s3" {
+//      bucket = "terraform-st-state"
+//      key = "Generic/terraform.tfstate"
+//      region = "us-east-1"
+//    }
+//  }
+//resource "aws_instance" "sample" {
+//  ami                   = "ami-074df373d6bafa625"
+//  instance_type         = "t3.micro"
+//  tags                  = {
+//    Name                = "Sample"
+//  }
+//}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraformstateremote"
+    key    = "sample/terraform.tfstate"
+    region = "us-east-1"
+    //dynamodb_table = "terraform"
   }
-  terraform {
-    backend "s3" {
-      bucket = "terraform-st-state"
-      key = "Generic/terraform.tfstate"
-      region = "us-east-1"
-    }
-  }
+}
