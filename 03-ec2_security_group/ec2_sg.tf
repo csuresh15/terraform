8d215a73a4b6b5f9963d432687530c6126d9b947
csuresh <sureshchilkuri@gmail.com>
2021-07-26 12:44:27 +0530

    practice1

---
+++ 03-ec2_security_group/ec2_sg.tf
@@ -0,0 +1,38 @@
+resource "aws_instance" "Generic" {
+  ami           = "ami-074df373d6bafa625"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = []
+
+  tags          = {
+    Name = "Generic"
+  }
+}
+resource "aws_security_group" "ssh_allow" {
+  name               = "allow_ssh"
+  description        = "Allow SSH inbound traffic"
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [0.0.0.0/0]
+     }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags                = {
+    Name              = "allow_ssh"
+  }
+}
+output "ec2-attributes" {
+  value = aws_security_group.ssh_allow
+}
+provider "aws" {
+  region = "us-east-1"
+}
