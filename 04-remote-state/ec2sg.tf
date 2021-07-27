42526f502e6ce7b5ac3aad5d6e1dfbebf16316a0
csuresh <sureshchilkuri@gmail.com>
2021-07-27 23:22:01 +0530

    practice1

--- 04-remote-state/ec2sg.tf
+++ 04-remote-state/ec2sg.tf
@@ -1,37 +1,39 @@
-resource "aws_instance" "Generic" {
-  ami           = "ami-074df373d6bafa625"
-  instance_type = "t3.micro"
-  tags        = {
-    Name       = "Generic"
-  }
-}
-//  provider "aws" {
-//    region      = "us-east-1"
-//  }
-//  terraform {
-//    backend "s3" {
-//      bucket = "terraform-st-state"
-//      key = "Generic/terraform.tfstate"
-//      region = "us-east-1"
-//    }
-//  }
-//resource "aws_instance" "sample" {
-//  ami                   = "ami-074df373d6bafa625"
-//  instance_type         = "t3.micro"
-//  tags                  = {
-//    Name                = "Sample"
+//resource "aws_instance" "Generic" {
+//  ami           = "ami-074df373d6bafa625"
+//  instance_type = "t3.micro"
+//  tags        = {
+//    Name       = "Generic"
 //  }
 //}
+//provider "aws" {
+//  region = "us-east-1"
+//}
+//terraform {
+//  backend "s3" {
+//    bucket = "terraformbkt01"
+//    key    = "sample/terraform.tfstate"
+//    region = "us-east-1"
+//    dynamodb_table = "terraformdb01"
+//    //dynamodb_table = "terraform"
+//  }
+//}
+resource "aws_instance" "sample" {
+  ami                   = "ami-074df373d6bafa625"
+  instance_type         = "t3.micro"
+  tags                  = {
+    Name                = "Sample"
+  }
+}
 
 provider "aws" {
   region = "us-east-1"
 }
+
 terraform {
   backend "s3" {
     bucket = "terraformbkt01"
     key    = "sample/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "terraformdb01"
-    //dynamodb_table = "terraform"
   }
 }
