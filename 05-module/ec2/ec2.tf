444fdec04adbad0dc610bd91661610536437503c
csuresh <sureshchilkuri@gmail.com>
2021-07-28 19:43:44 +0530

    practice1

--- 05-module/ec2/ec2.tf
+++ 05-module/ec2/ec2.tf
@@ -1,10 +1,14 @@
 resource "aws_instance" "Generic" {
-  ami = "ami-074df373d6bafa625"
-  instance_type = "t3.micro"
+  ami                    = "ami-074df373d6bafa625"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [var.SG_ID]
 
-  tags = {
-    Name = "Generic"
+  tags                   = {
+    Name                 = "Generic"
   }
 }
   variable "SG_ID" {}
+
+provider "aws" {
+  region                 = "us-east-1"
+}
