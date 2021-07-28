99e658c0712dafb94a790b37d003d7cf7bbb8ebb
csuresh <sureshchilkuri@gmail.com>
2021-07-28 23:27:28 +0530

    practice1

--- 05-module/ec2/ec2.tf
+++ 05-module/ec2/ec2.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "Generic" {
   ami                    = "ami-074df373d6bafa625"
-  instance_type          = [var.TYPE]
   count                  = 2
+  instance_type          = var.TYPE
   vpc_security_group_ids = [var.SG_ID]
 
   tags                   = {
-    Name                 = "Generic${count.index}"
+    Name                 = "Generic-${count.index}"
   }
 }
   variable "SG_ID" {}
